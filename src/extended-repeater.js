a363499a2419e00d055bbe9ba010e26e37666997
LidaChk <lvglazko@gmail.com>
2022-04-28 20:08:58 +0000

    feat: banana

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+function repeater(str, options) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  function irepeater(str, add, sep) {
+        return Array((!add)? 0: add).fill((!str)? '': str).join((!sep)? '' : sep);
+  }
+
+  return irepeater(str + irepeater(options.addition, options.additionRepeatTimes, options.additionSeparator),
+                     options.repeatTimes, options.separator);
 }
 
 module.exports = {
